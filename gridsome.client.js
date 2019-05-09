9811d25ed456963ef25e4318a7dd95b7564d8c1c
Adriaan van Rossum <git@adriaan.io>
2019-05-09 23:23:27 +0200

    Update to support Simple Analytics Vue plugin

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,8 +1,16 @@
-import VModal from 'vue-js-modal/dist/ssr.index'
+import SimpleAnalytics from "simple-analytics-vue";
 
-export default function (Vue, options, { isServer: disabled, router }) {
-  if(process.isClient) {
-    const { default: VModal } = require('vue-js-modal')
-    Vue.use(VModal)
+/**
+ * @param Vue                 Vue instance
+ * @param options             Plugin options
+ * @param context.appOptions  Options for the Vue instance
+ * @param context.router      The router instance
+ * @param context.head        VueMeta info objet
+ * @param context.isClient
+ * @param context.isServer
+ */
+export default function (Vue) {
+  if (process.isClient) {
+    Vue.use(SimpleAnalytics, { skip: process.env.NODE_ENV !== 'production' });
   }
 }
